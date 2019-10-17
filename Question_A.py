986c0e21f1c7cecc16f45f15c96252003fd09f53
Jeremi <jeremi.dodinh@gmail.com>
2019-10-17 18:38:54 -0400

    simplified

--- Question_A.py
+++ Question_A.py
@@ -8,9 +8,7 @@
 def main():
     l_1 = (1, 1)
     l_2 = (2, 6)
-    if (l_1[0] <= l_2[0] <= l_1[1]) or (l_1[0] <= l_2[1] <= l_1[1]):
-        print("The lines overlap")
-    elif (l_2[0] <= l_1[0] <= l_2[1]) or (l_2[0] <= l_1[1] <= l_2[1]):
+    if (l_1[0] <= l_2[0] <= l_1[1]) or (l_1[0] <= l_2[1] <= l_1[1]) or (l_2[0] <= l_1[0] <= l_2[1]) or (l_2[0] <= l_1[1] <= l_2[1]):
         print("The lines overlap")
     else:
         print("The lines don't overlap")
